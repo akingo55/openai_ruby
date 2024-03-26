f1ad5c426d1e763a9810d029a3e9b2282dda5cf6
akingo55 <am1157154@gmail.com>
2024-03-26 22:37:14 +0900

    refactoring

--- lib/notion_api.rb
+++ lib/notion_api.rb
@@ -1,19 +1,23 @@
 require 'notion-ruby-client'
 
 class NotionApi
+  class NoValidParameterError < StandardError; end
+
   def initialize
     @notion_api_token = ENV['NOTION_API_TOKEN']
     @notion_database_id = ENV['NOTION_DATABASE_ID']
   end
 
-  def get_database_pages
-    response = client.database_query(database_id: @notion_database_id, filter: empty_title_filter)
+  attr_reader :notion_api_token, :notion_database_id
+
+  def database_pages
+    response = client.database_query(database_id: notion_database_id, filter: empty_title_filter)
     descriptions(response[:results])
   end
 
-  def update_database_pages(page_id, parameters)
+  def update_database_pages(page_id, title, category)
     check_parameters(title, category)
-    
+
     properties = build_properties(title, category)
     client.update_page(page_id: page_id, properties: properties)
   end
@@ -21,12 +25,12 @@ class NotionApi
   private
 
   def client
-    Notion::Client.new(token: @notion_api_token)
+    Notion::Client.new(token: notion_api_token)
   end
 
   def descriptions(results)
     results.map do |r|
-      {page_id: r["id"], description: r["properties"]["Description"]["title"][0]["plain_text"]}
+      { id: r['id'], description: r['properties']['Description']['title'][0]['plain_text'] }
     end
   end
 
@@ -44,13 +48,13 @@ class NotionApi
   end
 
   def check_parameters(title, category)
-    raise "[Error] category:#{category} is not valid." unless valid_category?(category)
+    raise NoValidParameterError, "[Error] category:#{category} is not valid." unless valid_category?(category)
 
-    raise "[Error] title:#{title} is empty." if title.nil?
+    raise NoValidParameterError, '[Error] title is empty.' if title.nil?
   end
 
   def valid_category?(category)
-    categories = ['和食','洋食','中華','韓国']
+    categories = %w(和食 洋食 中華 韓国 その他)
     categories.include?(category)
   end
 
--- lib/openai_api.rb
+++ lib/openai_api.rb
@@ -1,24 +1,29 @@
 require 'openai'
 require 'retries'
 
-MAX_RETRY_COUNT = 3
-FUNCTION_NAME = 'recipe_analysis'.freeze
-
 class OpenaiApi
+  MAX_RETRY_COUNT = 3
+  FUNCTION_NAME = 'recipe_analysis'.freeze
+
   def initialize(user_text:)
-    @model = 'gpt-3.5-turbo'
+    @model = 'gpt-4-turbo-preview'
     @openai_api_key = ENV['OPENAI_API_KEY']
     @user_text = user_text.strip
   end
 
-  def get_chat_results
+  attr_reader :model, :openai_api_key, :user_text
+
+  def extract_structure_data
+    category_names = categories.map { |c| c[:name] }
+    category_explanation = categories.map { |c| [c[:name], c[:description]].join(':') }.join(',')
+
     response = with_retries(retry_option) do
       client.chat(
         parameters: {
-          model: @model,
+          model: model,
           temperature: 0,
           messages: build_messages,
-          functions: build_functions,
+          functions: build_functions(category_names, category_explanation),
           function_call: { name: FUNCTION_NAME }
         }
       )
@@ -36,7 +41,7 @@ class OpenaiApi
   private
 
   def client
-    OpenAI::Client.new(access_token: @openai_api_key)
+    OpenAI::Client.new(access_token: openai_api_key)
   end
 
   def retry_option
@@ -50,37 +55,39 @@ class OpenaiApi
 
   def build_messages
     introduction_text = "関数「#{FUNCTION_NAME}」を使って、以下のテキストからレシピの情報を抽出してください。\n"
-    [{ role: 'user', content: introduction_text + @user_text }]
+    [{ role: 'user', content: introduction_text + user_text }]
   end
 
   def categories
     [
       {
         name: '和食',
-        description: '日本発祥の料理（例：親子丼、うどん、肉じゃがなど）'
+        description: '日本の伝統的な食材と調理法を用いた料理。季節の変化を感じさせる味わいが特徴がある。（例：親子丼、うどん、肉じゃがなど）'
       },
       {
         name: '洋食',
-        description: '日本、中国、韓国以外の国発祥の料理（例：ハンバーグ、パスタなど）'
+        description: 'ヨーロッパの料理を基にした食文化。肉や魚を主に使い、ソースやスパイスで味を引き立てる。（例：ハンバーグ、パスタなど）'
       },
       {
         name: '中華',
-        description: '中国発祥の料理（例：麻婆豆腐、坦々麺、エビチリなど）'
+        description: '中国の伝統的な調理法を取り入れた料理。（例：麻婆豆腐、坦々麺、エビチリなど）'
       },
       {
         name: '韓国',
-        description: '韓国発祥の料理（例：キムチ、ビビンバ、サムギョプサルなど）'
+        description: '韓国の伝統的な料理。発酵食品や辛味が特徴。（例：キムチ、ビビンバ、サムギョプサルなど）'
+      },
+      {
+        name: 'その他',
+        description: '他の国や地域の料理に属さない、独自の味わいや文化を持つ料理。(例：タイ料理、メキシコ料理など)'
       }
     ]
   end
 
-  def build_functions
-    category_names = categories.map { |c| c[:name] }
-    category_explanation = categories.map { |c| [c[:name], c[:description]].join(':') }.join(',')
+  def build_functions(category_names, category_explanation)
     [
       {
         name: FUNCTION_NAME,
-        description: 'この関数は、テキストからレシピ名、レシピカテゴリを推定する処理です。titleにはレシピ名を、categoryには一番類似するカテゴリを選択してください。',
+        description: 'この関数は、テキストからレシピ名、レシピカテゴリを推定する処理です。titleにはレシピ名を、categoryには一番レシピに近いカテゴリを選択してください。合致するカテゴリがない場合は「その他」を選択してください。',
         parameters: {
           type: :object,
           properties: {
